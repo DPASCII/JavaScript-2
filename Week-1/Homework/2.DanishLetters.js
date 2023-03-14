525ad02ed68e512804cd2988b7d2af0e994af48f
DPASCII <davidcpascual@gmail.com>
2023-03-14 20:14:37 +0800

    Change code to not use regex, used for loop instead

--- Week-1/Homework/2.DanishLetters.js
+++ Week-1/Homework/2.DanishLetters.js
@@ -6,24 +6,40 @@ function danishLetters(string) {
     return;
   }
 
-  string.toLowerCase();
-
+  const input = string.toLowerCase().split("");
   let output = { total: 0 };
 
-  if (string.match("æ") !== null) {
-    output.æ = string.match(/æ/g).length;
-    output.total += output.æ;
-  }
-  if (string.match(/ø/g) !== null) {
-    output.ø = string.match(/ø/g).length;
-    output.total += output.ø;
-  }
-  if (string.match(/å/g) !== null) {
-    output.å = string.match(/å/g).length;
-    output.total += output.å;
+  for (let i = 0; i < string.length; i++) {
+    if (input[i] === "ø" || input[i] === "æ" || input[i] === "å") {
+      if (output[input[i]] > 0) {
+        output[input[i]] += 1;
+        output.total += 1;
+        continue;
+      }
+
+      output[input[i]] = 0;
+      output[input[i]] += 1;
+      output.total += 1;
+      continue;
+    }
   }
 
   console.log(output);
+
+  // if (string.match("æ") !== null) {
+  //   output.æ = string.match(/æ/g).length;
+  //   output.total += output.æ;
+  // }
+  // if (string.match(/ø/g) !== null) {
+  //   output.ø = string.match(/ø/g).length;
+  //   output.total += output.ø;
+  // }
+  // if (string.match(/å/g) !== null) {
+  //   output.å = string.match(/å/g).length;
+  //   output.total += output.å;
+  // }
+
+  // console.log(output);
 }
 
 const danishString = "Jeg har en blå bil";
