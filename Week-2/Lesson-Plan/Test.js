761adb70b6021d925aa25af96199cf192d670c47
DPASCII <davidcpascual@gmail.com>
2023-03-15 20:21:18 +0800

    Lesson Plan

---
+++ Week-2/Lesson-Plan/Test.js
@@ -0,0 +1,29 @@
+const names = [
+  "Carlo",
+  "Darvey",
+  "Jovey",
+  "David",
+  "Micmic",
+  "Clarisse",
+  "Cars",
+];
+
+names.forEach((name) => {
+  console.log(name);
+});
+
+const formalNames = names.map((name) => `Master ${name}`);
+
+console.log(formalNames);
+
+const guyNames = names
+  .filter((name) => {
+    const girls = ["Micmic", "Clarisse", "Cars"];
+    if (girls.indexOf(name) > -1) {
+      return false;
+    }
+    return true;
+  })
+  .map((name) => `Bobo ${name}`);
+
+console.log(guyNames);
