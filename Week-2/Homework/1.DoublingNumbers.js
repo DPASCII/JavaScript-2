3e1608fc8d2a93eee109c8d016011aaf48427bef
DPASCII <davidcpascual@gmail.com>
2023-03-17 22:32:56 +0800

    After discussion Changes

--- Week-2/Homework/1.DoublingNumbers.js
+++ Week-2/Homework/1.DoublingNumbers.js
@@ -1,10 +1,4 @@
-let numbers = [1, 2, 3, 4];
-let newNumbers = numbers
-  .filter((num) => {
-    if (num % 2 !== 0) {
-      return num;
-    }
-  })
-  .map((num) => num * 2);
+let numbers = [0, 1, 2, 3, 4];
+let newNumbers = numbers.filter((num) => num % 2 !== 0).map((num) => num * 2);
 
 console.log("The doubled numbers are", newNumbers); // [2, 6]
