61ab410166670445c03b7d2bae740188cf28b5b6
DPASCII <davidcpascual@gmail.com>
2023-03-22 19:02:00 +0800

    Discussion coding

--- Week-2/Lesson-Plan/Test.js
+++ Week-2/Lesson-Plan/Test.js
@@ -8,22 +8,29 @@ const names = [
   "Cars",
 ];
 
-names.forEach((name) => {
-  console.log(name);
+// names.forEach((name) => {
+//   console.log(name);
+// });
+
+// const formalNames = names.map((name) => `Master ${name}`);
+
+// console.log(formalNames);
+
+// const guyNames = names
+//   .filter((name) => {
+//     const girls = ["Micmic", "Clarisse", "Cars"];
+//     if (girls.indexOf(name) > -1) {
+//       return false;
+//     }
+//     return true;
+//   })
+//   .map((name) => `Bobo ${name}`);
+
+// console.log(guyNames);
+
+const initialString = "";
+const reducedNames = names.reduce((acc, cur) => {
+  console.log(acc);
+  console.log(cur);
+  return acc + ", " + cur;
 });
-
-const formalNames = names.map((name) => `Master ${name}`);
-
-console.log(formalNames);
-
-const guyNames = names
-  .filter((name) => {
-    const girls = ["Micmic", "Clarisse", "Cars"];
-    if (girls.indexOf(name) > -1) {
-      return false;
-    }
-    return true;
-  })
-  .map((name) => `Bobo ${name}`);
-
-console.log(guyNames);
